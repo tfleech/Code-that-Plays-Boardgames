a95122dd1b85843c1e6aca0925abc35eef58ce57
tfleech <tom96vb@gmail.com>
2018-07-30 21:56:34 -0400

    Added Game base class and TicTacToe game subclass

--- Base_Classes.py
+++ Base_Classes.py
@@ -1,1 +1,31 @@
 class Game:
+	def __init__(self, player1, player2):
+		self.player1 = player1
+		self.player2 = player2
+		self.board = None
+		self.turn = player1
+		self.gameover = False
+		self.winner = 0
+
+	def is_gameover(self):
+		"""
+		Set self.gameover and return answer
+		"""
+		pass
+
+	def make_move(self, move):
+		"""
+		Update self.board with new move
+		"""
+
+		if(self.turn == self.player1):
+			self.turn = self.player2
+		else:
+			self.turn = self.player1
+		self.is_gameover()
+
+	def is_move_valid(self, move):
+		pass
+
+	def print_board(self):
+		pass
---
+++ TicTacToe_game.py
@@ -0,0 +1,76 @@
+from Base_Classes import *
+import numpy as np
+
+class TicTacToe(Game):
+	def __init__(self, board=np.zeros(9)):
+		super().__init__(player1 = 1, player2 = -1)
+		self.board = board
+
+	def is_gameover(self):
+		test = (self.board[0] + self.board[1] + self.board[2]) == 3
+		test |= (self.board[3] + self.board[4] + self.board[5]) == 3
+		test |= (self.board[6] + self.board[7] + self.board[8]) == 3
+		test |= (self.board[0] + self.board[3] + self.board[6]) == 3
+		test |= (self.board[1] + self.board[4] + self.board[7]) == 3
+		test |= (self.board[2] + self.board[5] + self.board[8]) == 3
+		test |= (self.board[0] + self.board[4] + self.board[8]) == 3
+		test |= (self.board[2] + self.board[4] + self.board[6]) == 3
+		if test:
+			self.winner = self.player1
+			self.gameover = True
+			return True
+
+		#test = (self.board[0] + self.board[1] + self.board[2]) == 12
+		#test |= (self.board[3] + self.board[4] + self.board[5]) == 12
+		#test |= (self.board[6] + self.board[7] + self.board[8]) == 12
+		#test |= (self.board[0] + self.board[3] + self.board[6]) == 12
+		#test |= (self.board[1] + self.board[4] + self.board[7]) == 12
+		#test |= (self.board[2] + self.board[5] + self.board[8]) == 12
+		#test |= (self.board[0] + self.board[4] + self.board[8]) == 12
+		#test |= (self.board[2] + self.board[4] + self.board[6]) == 12
+		test = (self.board[0] + self.board[1] + self.board[2]) == -3
+		test |= (self.board[3] + self.board[4] + self.board[5]) == -3
+		test |= (self.board[6] + self.board[7] + self.board[8]) == -3
+		test |= (self.board[0] + self.board[3] + self.board[6]) == -3
+		test |= (self.board[1] + self.board[4] + self.board[7]) == -3
+		test |= (self.board[2] + self.board[5] + self.board[8]) == -3
+		test |= (self.board[0] + self.board[4] + self.board[8]) == -3
+		test |= (self.board[2] + self.board[4] + self.board[6]) == -3
+		if test:
+			self.winner = self.player2
+			self.gameover = True
+			return True
+
+		for i in range(9):
+			if self.board[i] == 0:
+				return False
+		self.gameover = True
+		self.winner = None
+		return True
+
+	def is_move_valid(self, move):
+		return self.board[move] == 0
+
+	def make_move(self, move):
+		if self.is_move_valid(move):
+			self.board[move] = self.turn
+
+		if (self.turn == self.player1):
+			self.turn = self.player2
+		else:
+			self.turn = self.player1
+
+		self.is_gameover()
+
+	def print_board(self):
+		for i in range(3):
+			s = ""
+			for j in range(3):
+				if self.board[j+3*i] == 0:
+					s+="_"
+				elif self.board[j+3*i] == self.player1:
+					s+="X"
+				else:
+					s+="O"
+			print(s)
+		print('\n')
